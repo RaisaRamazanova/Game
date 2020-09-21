bc1413d36052068f0c8cedda6f4ff3b5851ac823
RaisaRamazanova <ramazanova.raisat@yandex.ru>
2020-09-21 14:59:19 +0300

    Update ViewController.swift

--- Game/ViewController.swift
+++ Game/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController{
-    lazy var game: Concentration? = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
     var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
     var emojiChoices = ["😉", "😜", "🤢", "👻", "👺", "😈", "😀", "😼"]
     var emoji = [Int:String]()
@@ -17,7 +17,7 @@ class ViewController: UIViewController{
     @IBOutlet var cardButtons: [UIButton]!
 
     @IBAction func restartGame(_ sender: UIButton) {
-        game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+        game = Concentration(numberOfPairsOfCards: (cardButtons.count) / 2)
         self.viewDidLoad()
         self.viewWillAppear(true)
     }
@@ -26,7 +26,7 @@ class ViewController: UIViewController{
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
-            game!.chooseCard(at: cardNumber)
+            game.chooseCard(at: cardNumber)
             updateViewFromModel()
         } else {
             print("chosen card was not in cardButtons")
@@ -36,7 +36,7 @@ class ViewController: UIViewController{
     func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
-            let card = game!.cards[index]
+            let card = game.cards[index]
             if card.isFaceUp {
                 button.setTitle(emoji(for: card), for: UIControl.State.normal)
                 button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -49,8 +49,9 @@ class ViewController: UIViewController{
     
     func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+                var timeEmoji = emojiChoices
+                let randomIndex = Int(arc4random_uniform(UInt32(timeEmoji.count)))
+                emoji[card.identifier] = timeEmoji.remove(at: randomIndex)
         }
         return emoji[card.identifier] ?? "?"
     }
